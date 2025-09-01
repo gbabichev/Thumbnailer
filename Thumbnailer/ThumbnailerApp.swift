e777b7a8083099f731bb7c8d68df47677f148582
George Babichev <gbabichev1@gmail.com>
2025-09-01 09:21:17 -0400

    Initial Commit

---
+++ Thumbnailer/ThumbnailerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThumbnailerApp.swift
+//  Thumbnailer
+//
+//  Created by George Babichev on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct ThumbnailerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
